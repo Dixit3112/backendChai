bad111e729dd4667246bd825f7f064d6a8e2829f
Dixit3112 <dixit.dr123@gmail.com>
2024-07-16 15:53:36 +0530

    some changes of comments' places

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants.js";
+// improved version of "dotenv" is given below.
+// require('dotenv').config({path:'./env'});
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 
 dotenv.config({
     path: "./env"
 });
-// improved version of "dotenv" is given below.
-// require('dotenv').config({path:'./env'});
 
 const port = process.env.PORT || 8000;
 // console.log("Process.env of env file: ", process.env) // it gives the object of data of this project's.
@@ -26,41 +26,6 @@ connectDB()
 .catch((err) =>{
     console.log("Mongo connection failed!!", err);
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // use
